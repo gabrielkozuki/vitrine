f432e39c47fb54cab9bb63ce44df3d6f6c8c7da5
Gabriel Kozuki <gabrielkozuki@hotmail.com>
2023-05-22 18:58:26 -0300

    finish footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,22 +1,31 @@
 import './Footer.scss'
 import { useState, useEffect } from 'react'
-import { useNavigate } from "react-router-dom";
-import { icon } from '@fortawesome/fontawesome-svg-core/import.macro'
+import { useNavigate, Link } from "react-router-dom";
 
-import { } from '../api'
+import logo from '../assets/ifpr-logo.png'
 
 const Footer = () => {
   const navigateTo = useNavigate()
+  
   useEffect(() => {
   }, [])
 
   return (
     <div className='footer'>
-      <div className="row">
+      <div className="row" style={{height: '100%'}}>
         <div className="col">
-          <img src="" />
+          <img src={logo} />
         </div>
-        <div className="col"></div>
+        <div className="col">
+          <div className="row">
+            <p>2023 @ Feito por Gabriel Kozuki :)</p>
+            <div className="col social-media">
+              <i class="fa-brands fa-linkedin fa-3x" onClick={() => window.open('https://www.linkedin.com/in/gabriel-kozuki/', '_blank')}></i>
+              <i class="fa-brands fa-square-github fa-3x" onClick={() => window.open('https://github.com/gabrielkozuki', '_blank')}></i>
+              <i class="fa-brands fa-square-instagram fa-3x" onClick={() => window.open('https://www.instagram.com/kozuki1_/', '_blank')}></i>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
