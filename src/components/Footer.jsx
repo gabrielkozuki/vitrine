c4ad95a60af58cd956a5041ddc6df02671eaee45
Gabriel Kozuki <gabrielkozuki@hotmail.com>
2023-05-22 09:43:40 -0300

    footer component init

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
 import './Footer.scss'
 import { useState, useEffect } from 'react'
 import { useNavigate } from "react-router-dom";
+import { icon } from '@fortawesome/fontawesome-svg-core/import.macro'
 
 import { } from '../api'
 
@@ -11,6 +12,12 @@ const Footer = () => {
 
   return (
     <div className='footer'>
+      <div className="row">
+        <div className="col">
+          <img src="" />
+        </div>
+        <div className="col"></div>
+      </div>
     </div>
   )
 }
