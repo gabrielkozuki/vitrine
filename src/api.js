810ab9f1e3507310dbb19b54cea3ec430e0d09e3
Gabriel Kozuki <gabrielkozuki@hotmail.com>
2023-05-23 09:31:23 -0300

    api change

--- src/api.js
+++ src/api.js
@@ -1,7 +1,17 @@
+// API Doc - https://rapidapi.com/apidojo/api/unofficial-shein/
 
-const API_DNS = 'https://api.storerestapi.com'
+const API_DNS = 'https://api.storerestapi.com' // https://unofficial-shein.p.rapidapi.com
+const LOCATION = '&language=pt&country=BR&currency=BRL'
 
 export const getData = async(path) => {
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '5d374b6c22msh3872533098cbb45p132882jsn9f8bda8e7e07',
+            'X-RapidAPI-Host': 'unofficial-shein.p.rapidapi.com'
+        }
+    };
+
     try {
         let url = API_DNS + path
         const res = fetch(url)
